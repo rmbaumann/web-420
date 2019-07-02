80ee28d7e4a4f1774e17998cdf87cdf159d9f12c
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-01 20:45:14 -0500

    Create app.js

---
+++ api-gateway/app.js
@@ -0,0 +1,52 @@
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+
+
+// Database Connection
+mongoose.connect('mongodb+srv://web420admin:b3ll3vu3@buwebdev-cluster-1-opi0o.mongodb.net/api-gateway?retryWrites=true', {
+  promiseLibrary: require('bluebird')
+})
+.then ( () => console.log('connection established'))
+.catch( (err) => console.error(err));
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
