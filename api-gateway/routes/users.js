a85a32855e2b7ed146f1d4302e0917a414f9e9f1
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-10 15:20:27 -0500

    Update users.js

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,4 +1,21 @@
+ /*
+=============================================
+; Title: users.js
+; Author: Reva Baumann
+; Date: 08 July 2019
+; Modified by: Reva Baumann
+; Description: users.js
+;============================================
+*/
 
+/*
+Expected output:
+  Reva Baumann
+  users.js
+  08 July 2019
+*/
+
+// Start Program
 
 var express = require('express');
 var router = express.Router();
