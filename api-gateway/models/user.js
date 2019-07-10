731432cff5e70c35ee63296a34c96952dd1c0238
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-10 14:13:33 -0500

    Create user.js

---
+++ api-gateway/models/user.js
@@ -0,0 +1,29 @@
+ /*
+=============================================
+; Title: user.js
+; Author: Reva Baumann
+; Date: 08 July 2019
+; Modified by: Reva Baumann
+; Description: User model and Schema
+;============================================
+*/
+
+/*
+
+Expected Output
+
+
+*/
+
+// Start Program
+/** Fields Username, Password, Email */
+
+var mongoose = require('mongoose');
+var userSchema = new mongoose.Schema({
+    username: dianaprince,
+    password: lassooftruth,
+    email: invisiblejet@ww.com
+})
+module.exports = mongoose.model('dianaprince', userSchema);
+
+/** Database Requirements */
