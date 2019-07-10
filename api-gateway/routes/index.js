8f039676ea05d8f868e6edaf8749d979fc1b85cc
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-10 15:20:24 -0500

    Update index.js

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,4 +1,21 @@
+ /*
+=============================================
+; Title: index.js
+; Author: Reva Baumann
+; Date: 08 July 2019
+; Modified by: Reva Baumann
+; Description: index.js
+;============================================
+*/
 
+/*
+Expected output:
+  Reva Baumann
+  index.js
+  08 July 2019
+*/
+
+// Start Program
 
 var express = require('express');
 var router = express.Router();
