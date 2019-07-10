1523a54f74374e51be96f0db548c5e2a9a8bba13
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-10 15:20:18 -0500

    Update config.js

--- api-gateway/config.js
+++ api-gateway/config.js
@@ -1,3 +1,22 @@
+ /*
+=============================================
+; Title: config.js
+; Author: Reva Baumann
+; Date: 08 July 2019
+; Modified by: Reva Baumann
+; Description: config.js
+;============================================
+*/
+
+/*
+Expected output:
+  Reva Baumann
+  Config.js
+  08 July 2019
+*/
+
+// Start Program
+
 var config = {};
 config.web = {};
 config.web.port = process.env.PORT || '3000';
