b168ab92c0133d11735780deaf9ba8182a934232
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-24 18:12:59 -0500

    Update users.js

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -26,3 +26,11 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = {_id: id};
+  User.findById(query, callback);
+};
