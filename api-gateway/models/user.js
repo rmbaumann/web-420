4e802a1090975c16f271fe376247465cff1236bc
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-07-24 18:12:39 -0500

    Update user.js

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -18,12 +18,32 @@ Expected Output
 // Start Program
 /** Fields Username, Password, Email */
 
-var mongoose = require('mongoose');
-var userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String
+//Require Statements
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
+var mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true}
 });
-module.exports = mongoose.model('dianaprince', userSchema);
+
+module.exports = mongoose.model('User', userSchema);
+
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = {_id: id};
+  User.findById(query, callback);
+}
 
 /** Database Requirements */
+
+// end program
