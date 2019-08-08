64c88791b407a18ad81672d3d9c5b3a454d3680d
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-08-07 19:26:57 -0500

    Update user.js

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -8,42 +8,38 @@
 ;============================================
 */
 
-/*
-
-Expected Output
-
-
-*/
 
 // Start Program
-/** Fields Username, Password, Email */
 
-//Require Statements
-var User = require('../models/user');
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
-var config = require('../config');
-
+// Declare the mongoose variable and import the mongoose module
 var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Define the employee schema with a first and last name
 let userSchema = new Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
   email: {type: String, required: true}
 });
 
-module.exports = mongoose.model('User', userSchema);
-
+// Export the User model set to the user schema
+var model = mongoose.model('User', userSchema);
+module.exports = model;
 module.exports.add = (user, callback) => {
   user.save(callback);
 };
 
-module.exports.getById = (id, callback) => {
-  var query = {_id: id};
-  User.findById(query, callback);
+// Use findOne/getOne to recall user, defined by Username of individual
+module.exports.getOne = (e, callback) => {
+  query = { username: e };
+  model.findOne(query, callback);
 }
 
-/** Database Requirements */
+module.exports.getById = (id, callback) =>{
+  var query = {_id: id};
+  model.findById(query, callback);
+}
+
+
 
 // end program
